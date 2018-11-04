dfad66ea0a908dd2e551e96f21adf2f287a60800
Lexi <ambellinger24@gmail.com>
2018-11-03 22:41:22 -0500

    got the score screen to finally pop up; need to make the reset now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,42 +3,42 @@
 
 var questions = [
     {
-        question: "Question 1",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Option A",
-        gif: "assets/images/belding.gif",
+        question: "Which former player is affectionately dubbed 'Mr.Cub?'",
+        answers: ["Ernie Banks", "Ron Santo", "Sammy Sosa", "Ryne Sandberg"],
+        correctanswer: "Ernie Banks",
+        gif: "https://media.giphy.com/media/2EOQCjrCSiHcs/giphy.gif",
 
     }, {
-        question: "Question 2",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Option C",
-        gif: "assets/images/bulls.gif",
+        question: "What historic stadium do the Cubs call home?",
+        answers: ["Fenway Park", "Comiskey Park", "Wrigley Field", "Doublemint Dome"],
+        correctanswer: "Wrigley Field",
+        gif: "http://mlb.mlb.com/images/8/8/2/208022882/110316_wrigleyville_celebration_med_pk4e0iqm.gif",
     }, {
-        question: "Question 3",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Correct choice",
-        gif: $('#gif').append('<img src="TriviaGame\assets\images\fresh.gif" />'),
+        question: "Which Cubs outfielder holds 5 Gold Gloves and counting?",
+        answers: ["Albert Almora", "Dexter Fowler", "Kyle Schwarber", "Jason Heyward"],
+        correctanswer: "Jason Heyward",
+        gif: "https://media.giphy.com/media/ONYAPQhhTqvZTC7NU2/giphy.gif",
     }
 
     , {
-        question: "Question 4",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Correct choice",
-        gif: $('#gif').append('<img src="TriviaGame\assets\images\lionking.gif" />'),
+        question: "Which Cubs player goes by the nickname 'El Mago'?",
+        answers: ["Javier Báez", "Willson Contreras", "Criss Angel", "Albert Almora"],
+        correctanswer: "Javier Báez",
+        gif: "https://media.giphy.com/media/l0HlD1A4v1igVT4NW/giphy.gif",
     }
 
     , {
-        question: "Question 5",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Correct choice",
-        gif: $('#gif').append('<img src="TriviaGame\assets\images\nirvanabark.gif" />'),
+        question: "Which charitable Cubs player won the 2017 Roberto Clemente award?",
+        answers: ["Jon Lester", "Anthony Rizzo", "Carl Edwards, Jr.", "Kris Bryant"],
+        correctanswer: "Anthony Rizzo",
+        gif: "https://media.giphy.com/media/J3WYE0PGN5H3O/giphy.gif",
     }
 
     , {
-        question: "Question 6",
-        answers: ["Option A", "Option B", "Option C", "Option D"],
-        correctanswer: "Correct choice",
-        gif: $('#gif').append('<img src="TriviaGame\assets\images\skeeter.gif" />'),
+        question: "Prior to the 2016 World Series, when was the last time the Chicago Cubs won the championship?",
+        answers: ["1930", "1945", "1902", "1908"],
+        correctanswer: "1908",
+        gif: "https://media.giphy.com/media/3o7TKVJrotqrMJdlao/giphy.gif",
     }
 ]
 
@@ -77,19 +77,28 @@ function startgame() {
         $("#timer").html("Seconds Left: " + countdown);
 
         if (countdown === 0) {
+
             stop();
             unanswered++;
             console.log(countdown);
-            endgame();
+            
             //Next Question
             //Go to next question by adding to the index
             arrayIndex++;
             //Clear the buttons
             $("#answersarea").empty();
+            $("#question-area").empty();
+                    if(arrayIndex===6) {
+                        $("#answersarea").html("<h2>Final Scores</h2><p>Correct:" + correctanswer +   "</p> <p>Incorrect: " +incorrectAnswer+ "</p> <p>Unanswered: " + unanswered + "</p>");
+                        $("#timer").hide();
+                        $("<button>").html("restart");
+                    }
+                    
+
             //Retrieve question by calling the getQuestion function
             getQuestion()
             countdown = 31;
-            questionTime()
+            questionTime()               
         }
     }
 
@@ -97,12 +106,6 @@ function startgame() {
         clearInterval(intervalId);
     }
 
-    function endgame() {
-        if (arrayIndex > questions[arrayIndex]) {
-            $("#question-area").html("Game Over");
-            alert("game over");
-        }
-    }
 
     //get the question
     function getQuestion() {
@@ -111,10 +114,11 @@ function startgame() {
         questionTime();
 
         //Displaying the question 
-        $("#question-area").html(questions[arrayIndex].question);
+        
 
         //Dynamically create buttons for each of the potential answers
         for (var i = 0; i < questions[arrayIndex].answers.length; i++) {
+            $("#question-area").html(questions[arrayIndex].question);
             var button = $("<button>");
             // Adding a class of answers to our button
             button.addClass("answers-btn");
@@ -133,25 +137,30 @@ function startgame() {
             //if clicked answer ==== correct answer
             //display a "you're correct" message and gif
             //add a point to the score
-            if (checkAnswer === questions[arrayIndex].correctanswer) {
+            if (arrayIndex != 6 && checkAnswer === questions[arrayIndex].correctanswer) {
                 //display a "you're correct" message and gif
                 //add a point to the score
                 correctanswer++;
 
                 //Display correct screen with gifs
-                $("#question-area").html("That's correct!" + questions[arrayIndex].correctanswer + " is the correct answer!");
+                $("#question-area").html("That's right! " + questions[arrayIndex].correctanswer + " is the correct answer!");
                 $("#answersarea").html("<img src='" + questions[arrayIndex].gif + "' alt='gif'  />");
                 //Hide the timer
                 $("#timer").hide();
 
                 //Display correct screen for 3 seconds
                 threeSecondalert = setTimeout(function () {
-                    endgame();
                     //Next Question
                     //Go to next question by adding to the index
                     arrayIndex++;
                     //Clear the buttons area so that the buttons don't stack on each other
                     $("#answersarea").empty();
+                    $("#question-area").empty();
+                    if(arrayIndex===6) {
+                        $("#answersarea").html("<h2>Final Scores</h2><p>Correct:" + correctanswer +   "</p> <p>Incorrect: " +incorrectAnswer+ "</p> <p>Unanswered: " + unanswered + "</p>");
+                        $("#timer").hide();
+                        $("<button>").html("restart");
+                    }
                     //Retrieve question by calling the getQuestion function
                     getQuestion()
                     //restart the timer
@@ -163,27 +172,32 @@ function startgame() {
                     
 
 
-                }, 3000);
+                }, 4000);
 
                 //else if, if clicked answer does not === correct answer
-            } else if (checkAnswer !== questions[arrayIndex].correctanswer) {
+            } else if (arrayIndex != 6 && checkAnswer !== questions[arrayIndex].correctanswer) {
                 //disaplay a "you're wrong" message and gif
                 //add a point to the wrong score
                 incorrectAnswer++;
                 //Display incorrect screen with gifs
-                $("#question-area").html("Sorry! That's incorrect. " + questions[arrayIndex].correctanswer + " is the correct answer!");
-                $("#answersarea").html("gif here");
+                $("#question-area").html("Sorry! " + questions[arrayIndex].correctanswer + " is the correct answer!");
+                $("#answersarea").html("<img src='" + questions[arrayIndex].gif + "' alt='gif'  />");
                 //Hide the timer
                 $("#timer").hide();
 
                 //Display correct screen for 3 seconds
                 threeSecondalert = setTimeout(function () {
-                    endgame();
                     //Next Question
                     //Go to next question by adding to the index
                     arrayIndex++;
                     //Clear the buttons area so that the buttons don't stack on each other
                     $("#answersarea").empty();
+                    $("#question-area").empty();
+                    if(arrayIndex===6) {
+                        $("#answersarea").html("<h2>Final Scores</h2><p>Correct:" + correctanswer +   "</p> <p>Incorrect: " +incorrectAnswer+ "</p> <p>Unanswered: " + unanswered + "</p>");
+                        $("#timer").hide();
+                        $("<button>").html("restart");
+                    }
                     //Retrieve question by calling the getQuestion function
                     getQuestion()
                     //restart the timer
@@ -192,15 +206,24 @@ function startgame() {
                     $("#timer").show();
                     //return it to it's original time.
                     countdown = 31;
-                }, 3000);
+                   
+                }, 4000);
 
 
+            } else if (arrayIndex == 6){
+                alert("heyyyyy");
             }
-
+            
+            
+           
         });
+
+        
 
 
     }
     getQuestion();
 }
+
+
 startGameButton();
